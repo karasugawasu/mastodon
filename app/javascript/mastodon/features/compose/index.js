319b996b547e7ae65ae8e3c9b4c9fef18a56207f
karasugawasu <hotaruchan.saturn@gmail.com>
2021-06-07 15:37:01 +0900

    シングルカラムでのマスコット表示

--- app/javascript/mastodon/features/compose/index.js
+++ app/javascript/mastodon/features/compose/index.js
@@ -129,11 +129,9 @@ class Compose extends React.PureComponent {
 
             <ComposeFormContainer />
             <CureAnnouncementsContainer />
-            {multiColumn && (
-              <div className='drawer__inner__mastodon'>
-                <img alt='' draggable='false' src={mascot || elephantUIPlane} />
-              </div>
-            )}
+            <div className='drawer__inner__mastodon'>
+              <img alt='' draggable='false' src={mascot || elephantUIPlane} />
+            </div>
           </div>}
 
           <Motion defaultStyle={{ x: isSearchPage ? 0 : -100 }} style={{ x: spring(showSearch || isSearchPage ? 0 : -100, { stiffness: 210, damping: 20 }) }}>
